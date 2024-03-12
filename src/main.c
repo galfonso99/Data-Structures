3c2f488457ed67ffa4c93404d5fd58ee38d28fae
Gabriel Alfonso <gabrielalfonso99@gmail.com>
2024-03-12 13:09:10 -0400

    Somehow I managed to implement a Queue using only a LinkedList? Without using a Queue struct

--- src/main.c
+++ src/main.c
@@ -5,34 +5,43 @@
 #include <stdlib.h>
 #include "linked_list.h"
 
+ListNode* head_and_tail_from_array (ListNode* list, int* array, int length) {
+  if (length == 0) {return NULL;}
+  list->val = array[0];
+  for (int i = 1; i < length; i++) {
+    ListNode* next_node = new_list(-1);
+    list->next = next_node;
+    list = list->next;
+    list->val = array[i];
+  }
+  return list;
+}
+
+void enqueue (ListNode **tail, int val) {
+  (*tail)->next = new_list(val);
+  (*tail) = (*tail)->next;
+} 
+
+void deque (ListNode* list) {
+  *list = *(list->next);
+}
+
+int peek (ListNode* list) {
+  return list->val;
+}
 
 int main(int argc, char **argv) {
   // int array[15] = {4,6,8, 12, 14, 15, 16, 17, 25, 27, 35, 46, 52, 55, 62};
   int array[15] = {45, 23, 62,  58, 38, 8543, 734, 85,
                    34, 54, 856, 27, 55, 9375, 845};
   int length = sizeof(array) / sizeof(int);
-  ListNode* list = list_from_array(array, length);
-  ListNode* head = list;
-  // printf("%lu\n", get_length(list));
-  // print_list(list);
-  // prepend(list, -1111);
-  // append(list, 16788);
-  // add(list, 77777);
-  // print_list(list);
-  // printf("%lu\n", get_length(list));
-  // ListNode* new_list = get_index(list, 5);
-  // printf("%d\n", new_list->val);
-  // printf("%lu\n", get_length(new_list));
-  // insert_at(list, 14, -77777);
-  // print_list(list);
-  // remove_head(list);
-  remove_at(list, 3);
-  // print_list(list);
-  remove_at(list, 13);
+  ListNode* list = new_list(0);
+  // head_and_tail_from_array returns tail and modifies list in place
+  ListNode* tail = head_and_tail_from_array(list, array, length);
+  // print_list(tail);
+  // printf("%d\n", peek(list));
+  enqueue(&tail, 537386);
+  enqueue(&tail, -444837);
+  deque(list);
   print_list(list);
-  printf("%lu\n", get_length(list));
-  // for (int i = 0; i < length; i++) {
-  //   printf("%d\n", array[i]);
-  // }
-  // printf("%d\n", index);
 }
