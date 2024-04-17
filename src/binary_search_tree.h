8929847abd3f149e71eec65581c99c423eb46032
Gabriel Alfonso <gabrielalfonso99@gmail.com>
2024-04-17 14:39:59 -0400

    Implemented insert and delete for Tries

--- src/binary_search_tree.h
+++ src/binary_search_tree.h
@@ -188,105 +188,6 @@ int get_height(binary_node *node) {
     return max(get_height(node->left), get_height(node->right)) + 1;
 }
 
-binary_node * get_inorder_predecessor (binary_node * root) {
-    binary_node * pred_parent = root;
-    binary_node * pred = root->left;
-    while (pred->right != NULL) {
-        pred_parent = pred;
-        pred = pred->right;
-    }
-    if (pred_parent != root) {
-        pred_parent->right = pred->left;   
-    }
-    else {
-        pred_parent->left = pred->left;
-    }
-
-    root->val = pred->val;
-    return root;
-}
-
-binary_node * get_inorder_successor (binary_node * root) {
-    binary_node * succ_parent = root;
-    binary_node * succ = root->right;
-    while (succ->left != NULL) {
-        succ_parent = succ;
-        succ = succ->left;
-    }
-    if (succ_parent == root) {
-        succ_parent->right = succ->right;
-    }
-    // There is an edge case where if succ_parent and succ have the same value, then the resulting BST
-    // Will have a Node with its right child having the same value as the parent and this breaks the rules
-    // of my BST (equal values go to the left and only higher values go to the right). For instance
-    //          5
-    //        /  \
-    //      5     6
-    //           / 
-    //          6
-    // If we try to remove 5 (the top root) the left child of the first 6 gets removed and the value of the second
-    // 6 gets copied to where 5 was and we end up with
-    //          6
-    //        /  \
-    //      5     6
-    // Which as you can see breaks the rules of this BST by having a 6 to the right of a 6 which should be implossible
-    // and that is why the following case check is necessary
-    // If the inorder successor is exactly 2 nodes deep and succ and succ_parent have equal value, then
-    else if (succ->val == root->right->val) {
-        succ_parent->left = NULL;
-        binary_node * root_right = root->right;
-        root->right = root->right->right;
-        root_right->right = NULL;
-        root->val = succ->val;
-        binary_node * child = root->left;
-        while (child->right != NULL) {
-            child = child->right;
-        }
-        child->right = root_right;
-    }
-    // If succ_parent does not equal root
-    else {
-        succ_parent->left = succ->right;   
-    }
-    root->val = succ->val;
-    return root;
-}
-
-
-binary_node * delete_node (binary_node * root, int value) {
-    if (root == NULL) return root;
-    if (root->val > value) {
-        root->left = delete_node(root->left, value);
-        return root;
-    }
-    else if (root->val < value) {
-        root->right = delete_node(root->right, value);
-        return root;
-    }
-    if (root->left == NULL) {
-        binary_node * temp = root->right;
-        return temp;
-    }
-    else if (root->right == NULL) {
-        binary_node * temp = root->left;
-        return temp;
-    }
-
-    else {
-        int left_h = get_height(root->left) + 1;
-        int right_h = get_height(root->right) + 1;
-        bool left_is_deeper = left_h >= right_h;
-        binary_node * inorder_child;
-        if (left_is_deeper) {
-            inorder_child = get_inorder_predecessor(root);
-        }
-        else {
-            inorder_child = get_inorder_successor(root);
-        }
-        return inorder_child;
-    }
-}
-
 binary_node * get_biggest_child (binary_node ** node, binary_node *parent) {
     int val;
     binary_node * biggest_node = NULL;
@@ -373,6 +274,108 @@ bool delete_node_old (binary_node * head, int value) {
     }
     return true;
 }
+
+binary_node * get_inorder_predecessor (binary_node * root) {
+    binary_node * pred_parent = root;
+    binary_node * pred = root->left;
+    while (pred->right != NULL) {
+        pred_parent = pred;
+        pred = pred->right;
+    }
+    if (pred_parent != root) {
+        pred_parent->right = pred->left;   
+    }
+    else {
+        pred_parent->left = pred->left;
+    }
+
+    root->val = pred->val;
+    return root;
+}
+
+binary_node * get_inorder_successor (binary_node * root) {
+    binary_node * succ_parent = root;
+    binary_node * succ = root->right;
+    while (succ->left != NULL) {
+        succ_parent = succ;
+        succ = succ->left;
+    }
+    if (succ_parent == root) {
+        succ_parent->right = succ->right;
+    }
+    // There is an edge case where if succ_parent and succ have the same value, then the resulting BST
+    // Will have a Node with its right child having the same value as the parent and this breaks the rules
+    // of my BST (equal values go to the left and only higher values go to the right). For instance
+    //          5
+    //        /  \
+    //      5     6
+    //           / 
+    //          6
+    // If we try to remove 5 (the top root) the left child of the first 6 gets removed and the value of the second
+    // 6 gets copied to where 5 was and we end up with
+    //          6
+    //        /  \
+    //      5     6
+    // Which as you can see breaks the rules of this BST by having a 6 to the right of a 6 which should be implossible
+    // and that is why the following case check is necessary
+    // If the inorder successor is exactly 2 nodes deep and succ and succ_parent have equal value, then
+    else if (succ->val == root->right->val) {
+        succ_parent->left = NULL;
+        binary_node * root_right = root->right;
+        root->right = root->right->right;
+        root_right->right = NULL;
+        binary_node * child = root->left;
+        while (child->right != NULL) {
+            child = child->right;
+        }
+        child->right = root_right;
+    }
+    // If succ_parent does not equal root
+    else {
+        succ_parent->left = succ->right;   
+    }
+    root->val = succ->val;
+    free(succ);
+    return root;
+}
+
+
+binary_node * delete_node (binary_node * root, int value) {
+    if (root == NULL) return root;
+    if (root->val > value) {
+        root->left = delete_node(root->left, value);
+        return root;
+    }
+    else if (root->val < value) {
+        root->right = delete_node(root->right, value);
+        return root;
+    }
+    if (root->left == NULL) {
+        binary_node * temp = root->right;
+        free(root);
+        return temp;
+    }
+    else if (root->right == NULL) {
+        binary_node * temp = root->left;
+        free(root);
+        return temp;
+    }
+
+    else {
+        int left_h = get_height(root->left) + 1;
+        int right_h = get_height(root->right) + 1;
+        bool left_is_deeper = left_h >= right_h;
+        binary_node * inorder_child;
+        if (left_is_deeper) {
+            inorder_child = get_inorder_predecessor(root);
+        }
+        else {
+            inorder_child = get_inorder_successor(root);
+        }
+        return inorder_child;
+    }
+}
+
 
 void binary_search_tree_driver () {
     int arr[7] = {7, 4, 12, 2, 5, 9, 15};
--- src/main.c
+++ src/main.c
@@ -5,13 +5,91 @@
 #include <string.h>
 #include <math.h>
 // #include "binary_tree.h"
-#include "binary_search_tree.h"
+// #include "binary_search_tree.h"
 
+ typedef struct trie {
+    char ch;
+    struct trie * children[26];
+    bool is_word;
+ } Trie;
+
+int index_of (char ch) {
+    int ascii = (int) ch;
+    int index = ascii - ((int) 'a');
+    return index;
+}
+
+Trie * create_trie (char ch) {
+    Trie * trie = malloc(sizeof(Trie));
+    trie->ch = ch;
+    trie->is_word = false;
+    return trie;
+}
+
+void print_arr (Trie * arr[26]) {
+    for (int i = 0; i < 26; i++) {
+        if (arr[i]) {
+            printf("%c\t", arr[i]->ch);
+        }
+        else {
+            printf("NULL\t");
+        }
+    }
+}
+
+bool insert (Trie * trie, char * word, int word_length) {
+    for (int i = 0; i < word_length; i++) {
+        char curr_ch = word[i];
+        int index = index_of(curr_ch);
+        if (trie->children[index]) {
+            continue;
+        }
+        trie->children[index] = create_trie(curr_ch);
+        if (i == word_length - 1) {
+            trie->is_word = true;
+        }
+        trie = trie->children[index];
+    }
+    return true;
+}
+
+bool delete_word (Trie * trie, Trie * parent, char * word) {
+    if (parent && parent->is_word) {
+        return true;
+    }
+    delete_word(trie->children[index_of(*word)], trie, word+1);
+    // print_arr(trie->children);
+    if (parent) parent->children[index_of(trie->ch)] = NULL;
+    return true;
+}
+
+// Starting now with tries
 int main (int argc, char **argv) {
     // int arr[13] = {12, 54, 15, 61, 77, 33, 34, 78, 80, 91, 100, 55, 77};
     int arr[7] = {7, 4, 12, 2, 5, 9, 15};
     int length = sizeof(arr) / sizeof(int);
-    binary_search_tree_driver();
+    printf("%d\n", index_of('g'));
+    Trie * trie = create_trie('_');
+    insert(trie, "car", 3);
+    Trie * copy = trie;
+    // printf("%c, %c, %c", trie->children[index_of('c')]->ch, trie->children[index_of('a')]->ch, 
+    //        trie->children[index_of('r')]->ch);
+    // print_arr(trie->children);
+    // printf("END\n");
+    // trie = trie->children[index_of('c')];
+    // print_arr(trie->children);
+    // printf("END\n");
+    // trie = trie->children[index_of('a')];
+    // print_arr(trie->children);
+    // printf("END\n");
+    // trie = trie->children[index_of('r')];
+    // print_arr(trie->children);
+    // printf("END\n");
+
+    delete_word(copy, NULL, "car");
+    print_arr(copy->children);
+    printf("END\n");
+    // binary_search_tree_driver();
 }
 
 // for (int i = 0; i < length; i++) {
